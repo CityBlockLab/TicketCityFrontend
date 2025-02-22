93a37329a1f4b9ffb77d5f862735954d0497858f
kemsguy7 <mattidungafa@gmail.com>
2025-02-22 13:20:09 +0100

    downgraded to tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
-// tailwind.config.js
-module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-      extend: {
-        colors: {
-          background: '#090014',
-          searchBg: '#393141',
-          borderStroke: '#3A3A3A',
-          textGray: '#A5A5A5',
-          textWhite: '#FFFFFF',
-          primary: '#FF8A00',
-          secondary: '#FFD5A4',
-        },
-        fontFamily: {
-          inter: ['Inter', 'sans-serif'],
-          orbitron: ['Orbitron', 'sans-serif'],
-          poppins: ['Poppins', 'sans-serif'],
-          roboto: ['Roboto', 'sans-serif'],
-        },
-        backgroundImage: {
-          'button-gradient': 'linear-gradient(128.92deg, #FFD5A4 0.66%, #FF8A00 110.05%)',
-        },
-        boxShadow: {
-          'button-inset': '1px 1px 10px 0px #FFFFFF40 inset',
-        },
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: '#090014',
+        searchBg: '#393141',
+        borderStroke: '#3A3A3A', 
+        textGray: '#A5A5A5',
+        textWhite: '#FFFFFF',
+        primary: '#FF8A00', 
+        secondary: '#FFD5A4',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        orbitron: ['Orbitron', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      backgroundImage: {
+        'button-gradient': 'linear-gradient(128.92deg, #FFD5A4 0.66%, #FF8A00 110.05%)',
+      },
+      boxShadow: {
+        'button-inset': '1px 1px 10px 0px #FFFFFF40 inset',
       },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
